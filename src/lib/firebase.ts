2fd4c52056796889d5f1fcede73442aefb43152c
Nishant Varandani <m.simplynish@gmail.com>
2025-07-28 06:34:06 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  "projectId": "mealwhiz-fr88z",
+  "appId": "1:151289399985:web:d5aed2e017ff755a98f090",
+  "storageBucket": "mealwhiz-fr88z.appspot.com",
+  "apiKey": "AIzaSyAyx2KMRBsbYYCmWKtXhjYunWytvzRYImM",
+  "authDomain": "mealwhiz-fr88z.firebaseapp.com",
+  "messagingSenderId": "151289399985"
 };
 
 // Initialize Firebase
